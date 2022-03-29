8f371717a9fd5988815139958b8e40605bf17c36
Shurel Reynolds <shurel_reynolds@yahoo.com>
2022-03-29 10:50:14 +0100

    inheritance etc

---
+++ src/main/java/com/shurel/fruit/Fruit.java
@@ -0,0 +1,12 @@
+package com.shurel.fruit;
+
+public class Fruit{
+    protected String name;
+    protected String color;
+    protected String countryOfOrigin;
+
+    public String getDetails(){
+        return name+" "+color+" "+countryOfOrigin;
+    }
+}
+
